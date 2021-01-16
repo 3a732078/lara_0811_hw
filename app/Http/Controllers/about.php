4d79d8664d54f433ac8c1138a82e8d2fe8c07972
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-16 10:48:59 +0800

    寫入controller 回傳blade

--- app/Http/Controllers/about.php
+++ app/Http/Controllers/about.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class about extends Controller
 {
     //
+    public function index(){
+        return view('about');
+    }
 }
--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class home extends Controller
 {
     //
+    public function index(){
+        return view('home');
+    }
 }
